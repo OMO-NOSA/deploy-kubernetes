f9ef521e5541372b58457489bd4e93045a2f938c
nosa.omorodion <nosa.omorodion@sumup.com>
2023-06-26 07:18:40 +0200

    Added aws and kubernetes provider

--- providers.tf
+++ providers.tf
@@ -1,1 +1,15 @@
 #Provider Definition for both AWS and Kubernetes
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.47"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.10"
+    }
+  }
+}
