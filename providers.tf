b37b99ef8458b5274e68611a253da700fd607293
nosa.omorodion <nosa.omorodion@sumup.com>
2023-06-26 07:29:14 +0200

    Added provider definition for kubernetes and AWS

--- providers.tf
+++ providers.tf
@@ -1,15 +1,20 @@
 #Provider Definition for both AWS and Kubernetes
-terraform {
-  required_version = ">= 1.0"
+# Provider Definition for both AWS and Kubernetes
+provider "aws" {
+  region     = local.region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.47"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.10"
-    }
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,1 +1,12 @@
 # Terraform Variables Definition
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+  sensitive   = true
+}
