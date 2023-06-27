26942ec6307538bae49ebacf1252c1876aeff7a7
nosa.omorodion <nosa.omorodion@sumup.com>
2023-06-27 23:30:45 +0200

    Added variables definitions for locals

--- locals.tf
+++ locals.tf
@@ -2,15 +2,15 @@ data "aws_availability_zones" "available" {}
 data "aws_caller_identity" "current" {}
 
 locals {
-  name   = "ex-${replace(basename(path.cwd), "_", "-")}"
-  region = "eu-west-1"
+  name   = var.name
+  region = var.region
 
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
   tags = {
-    Example    = local.name
-    GithubRepo = "terraform-aws-eks"
-    GithubOrg  = "terraform-aws-modules"
+    Name        = local.name
+    Environment = var.environment
+
   }
 }
--- variables.tf
+++ variables.tf
@@ -10,3 +10,21 @@ variable "aws_secret_key" {
   type        = string
   sensitive   = true
 }
+
+variable "region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "name" {
+  description = "Name to be used on all the resources as identifier"
+  type        = string
+  default     = "aws-eks-deploy"
+}
+
+variable "environment" {
+  description = "Environment to be used on all the resources as identifier"
+  type        = string
+  default     = "dev"
+}
