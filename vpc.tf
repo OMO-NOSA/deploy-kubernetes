62447a1a467e5c5517d0cdded853589bfdc6ff8b
nosa.omorodion <nosa.omorodion@sumup.com>
2023-06-27 23:39:30 +0200

    created security group and IAM policy

--- vpc.tf
+++ vpc.tf
@@ -23,3 +23,38 @@ module "vpc" {
 
   tags = local.tags
 }
+
+resource "aws_security_group" "default-sec" {
+  name_prefix = "${local.name}-default-sec"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = [
+      "10.0.0.0/8",
+      "172.16.0.0/12",
+      "192.168.0.0/16",
+    ]
+  }
+
+  tags = merge(local.tags, { Name = "${local.name}-default-sec" })
+}
+
+resource "aws_iam_policy" "EC2Describe" {
+  name = "${local.name}-EC2Describe"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
